ee8d61e6aec732b868571e6e732ec04882fe997f
Phumin <poolmean602@gmail.com>
2021-12-19 02:12:02 +0700

    add reserve api and room api

--- src/reserve/reserve.controller.ts
+++ src/reserve/reserve.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, Post, Put, Req } from "@nestjs/common";
+import { Request } from "express";
 
-@Controller('reserve')
-export class ReserveController {}
+import * as rs from "./dto/reserve.dto";
+import { Reserve } from "./interfaces/reserve.interface";
+import { ReserveService } from "./reserve.service";
+
+@Controller("reserve")
+export class ReserveController {
+  constructor(private _reserveService: ReserveService) {}
+
+  @Post()
+  async createReserve(@Req() request: Request): Promise<Reserve> {
+    const createReserve = request.body as rs.createReserveDto;
+    return await this._reserveService.createReserve(createReserve);
+  }
+
+  @Put()
+  async editReserve(@Req() request: Request): Promise<Reserve> {
+    const editReserve = request.body as rs.editReserveDto;
+    return await this._reserveService.editReserve(editReserve);
+  }
+
+  @Delete(":id")
+  async cancelReserve(@Param("id") id: string): Promise<Reserve> {
+    return await this._reserveService.cancelReserve(id);
+  }
+}
--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -2,7 +2,7 @@ import { Model } from "mongoose";
 import { Injectable, Inject } from "@nestjs/common";
 
 import { Room } from "./interfaces/room.interface";
-import { CreateRoomDto } from "./dto/room.dto";
+import * as r from "./dto/room.dto";
 
 @Injectable()
 export class RoomsService {
@@ -11,12 +11,22 @@ export class RoomsService {
     private roomModel: Model<Room>
   ) {}
 
-  async createRoom(createRoomDto: CreateRoomDto): Promise<Room> {
-    const createdRoom = new this.roomModel(createRoomDto);
-    return await createdRoom.save();
+  async createRoom(createRoomDto: r.CreateRoomDto): Promise<Room> {
+    const room = {
+      number: createRoomDto.number,
+      quantity: createRoomDto.quantity,
+      type:
+        createRoomDto.type == 0 ? r.roomType.Lecture : r.roomType.Laboratory,
+    } as Room;
+    const createRoom = new this.roomModel(room);
+    return await createRoom.save();
   }
 
   async getRoom(number: string): Promise<Room> {
     return await this.roomModel.findOne({ number: number }).exec();
+  }
+
+  async getRooms(): Promise<Room[]> {
+    return await this.roomModel.find().exec();
   }
 }
