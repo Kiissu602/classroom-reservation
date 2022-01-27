ead881f20b2803a0baacb5af5c9acd35db549eed
Phumin <poolmean602@gmail.com>
2022-01-28 02:07:03 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
